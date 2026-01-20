1f486cb5f3006c7e37fb38d7cf0ff97b35485711
Shivam Jha <jha24978@gmail.com>
2026-01-20 18:07:29 +0530

    fixed

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const isLoggedIn = request.cookies.get("accessToken")?.value || request.cookies.get("refreshToken")?.value;
+  const isLoggedIn = request.cookies.get("token")?.value;
   console.log("isLoggedIn", isLoggedIn);
 
   const protectedRoutes = [
